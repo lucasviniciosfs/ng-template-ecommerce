cb448275d712de4c5945cc20678751af116645c1
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2020-10-09 18:47:54 -0300

    carregando component

---
+++ src/app/shared/carregando/carregando.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carregando',
+  templateUrl: './carregando.component.html',
+  styleUrls: ['./carregando.component.scss']
+})
+export class CarregandoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
